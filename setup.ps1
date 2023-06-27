ac3cad2e53d4f1e743b97675db3b4b9a0e48e18c
Paul Ayoo <paulayoo@microsoft.com>
2023-06-27 16:32:16 +0300

    updated customers

--- setup.ps1
+++ setup.ps1
@@ -7,28 +7,30 @@
  if (-not $isAheadOrSame){
      Write-Host "WARNING!! Your local branch is behind origin/$($targetBranch), please rebase or pull the latest artifacts from the remote $($targetBranch) branch`n" -ForegroundColor RED
     }
-elseif ($conflictingChanges) {
+if ($conflictingChanges) {
     Throw "Conflicting changes detected between current branch and remote $($targetBranch), please rebase or pull the latest artifacts from the remote $($targetBranch) branch`n"
     exit 1
     }
-    else {   
-        Write-Host "Local branch is up to date with $($targetBranch) branch."
-        write-host "============================================`n"
-    }
 
+    
     # check for changes in Tooling and Customer folders
  $directoriesToCheck = @(
-     "internal/*",   # changes in permissions, ms graph preautz, tooling etc
-      "customers/",   # customer facing changes
-      "tools/*"
-    )
+    "internal/*",   # changes in permissions, ms graph preautz, tooling etc
+     "customers/*",   # customer facing changes
+     "tools/*"
+   )
 
- $modifiedFilesInTargetBranch = git diff --name-only origin/$targetBranch
+$modifiedFilesInTargetBranch = git diff --name-only origin/$targetBranch
 
- foreach ($directory in $directoriesToCheck) {
-     $modifiedFile = $modifiedFilesInTargetBranch  | Where-Object { $_ -like $directory }
-     if ($modifiedFile) {
-          Throw "Changes in internal tools and/or customer binaries detected, please rebase or pull the latest artifacts from the remote $($targetBranch) branch`n"
-         exit 1
-        }
+foreach ($directory in $directoriesToCheck) {
+    $modifiedFile = $modifiedFilesInTargetBranch  | Where-Object { $_ -like $directory }
+    if ($modifiedFile) {
+        Write-Error "Changes in internal tools and/or customer binaries detected, please rebase or pull the latest artifacts from the remote $($targetBranch) branch`n"
+        exit 1
+       }
+   }
+
+if (-not $conflictingChanges && $isAheadOrSame) {   
+        Write-Host "Local branch is up to date with $($targetBranch) branch."
+        write-host "============================================`n"
     }
